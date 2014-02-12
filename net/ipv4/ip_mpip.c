e7f36ec856fb6d553c61c0aa0b77e41b9f2dd790
Bill <gbtian@gmail.com>
2014-02-11 22:15:30 -0500

    submit

--- net/ipv4/ip_mpip.c
+++ net/ipv4/ip_mpip.c
@@ -166,7 +166,6 @@ void mpip_tcp_v4_send_check(struct sk_buff *skb, __be32 saddr, __be32 daddr)
 {
 	struct iphdr *iph = (struct iphdr *)skb_network_header(skb);
 	struct tcphdr *th = (struct tcphdr *)((__u32 *)iph + iph->ihl);
-	th->check = 0;
 //	printk("saddr=%d, %d\n", saddr, __LINE__);
 //	printk("daddr=%d, %d\n", daddr, __LINE__);
 //	printk("skb->ip_summed=%d\n", skb->ip_summed);
@@ -495,12 +494,12 @@ int process_mpip_options(struct sk_buff *skb)
 		skb_reset_network_header(skb);
 		iph = (struct iphdr *)skb_network_header(skb);
 		iph->ihl -= opt->optlen>>2;
-//		if(iph->protocol==IPPROTO_TCP)
-//		{
-//			mpip_tcp_v4_send_check(skb, iph->saddr, iph->daddr);
-//		}
-
 		iph->tot_len = htons(skb->len);
+		if(iph->protocol==IPPROTO_TCP && sysctl_mpip_rcv)
+		{
+			__tcp_v4_send_check(skb, iph->saddr, iph->daddr);
+		}
+
 		iph->check = 0;
 		iph->check = ip_fast_csum((unsigned char *)iph, iph->ihl);
 
