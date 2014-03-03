a9f15f0c6ae8e03fd192c44b8361dd3a9ae7ebfa
Bill <gbtian@gmail.com>
2014-03-03 18:37:22 -0500

    submit

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -102,6 +102,7 @@ int __ip_local_out(struct sk_buff *skb)
 		insert_mpip_options(skb, false);
 	}
 
+	iph = ip_hdr(skb);
 	iph->tot_len = htons(skb->len);
 	ip_send_check(iph);
 	return nf_hook(NFPROTO_IPV4, NF_INET_LOCAL_OUT, skb, NULL,
