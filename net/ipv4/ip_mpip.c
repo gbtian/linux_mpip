65d0ab11a99452b9649a084c2847fe1ebdaddc3a
Bill <gbtian@gmail.com>
2014-03-06 20:35:03 -0500

    submit

--- net/ipv4/ip_mpip.c
+++ net/ipv4/ip_mpip.c
@@ -815,18 +815,14 @@ int insert_mpip_options(struct sk_buff *skb, struct flowi *fl, bool pushed)
 {
 	struct sock *sk = skb->sk;
 	struct inet_sock *inet = inet_sk(sk);
-	struct ip_options_rcu *inet_opt;
-	struct flowi4 *fl4;
-	struct rtable *rt;
-	struct iphdr *iph;
 	int res;
 
-	if (inet->inet_opt && inet->inet_opt->opt.optlen)
-	{
-
-		mpip_log("%d, %s, %d\n", inet->inet_opt->opt.optlen, __FILE__, __LINE__);
-		return 0;
-	}
+//	if (inet->inet_opt && inet->inet_opt->opt.optlen)
+//	{
+//
+//		mpip_log("%d, %s, %d\n", inet->inet_opt->opt.optlen, __FILE__, __LINE__);
+//		return 0;
+//	}
 
 	//options = kzalloc(MPIP_OPT_LEN, GFP_ATOMIC);
 
