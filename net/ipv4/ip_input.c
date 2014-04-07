7db1064638541d5fe9699bf713798565d2ddd584
Bill <gbtian@gmail.com>
2014-04-07 18:57:50 -0400

    submit

--- net/ipv4/ip_input.c
+++ net/ipv4/ip_input.c
@@ -389,9 +389,7 @@ static int ip_rcv_finish(struct sk_buff *skb)
 
 	if (sysctl_mpip_enabled && iph->protocol == IPPROTO_TCP)
 	{
-		unsigned char session_id = get_session(skb);
-//		if (session_id > 0 && add_to_tcp_skb_buf(skb, session_id))
-//			return NET_RX_SUCCESS;
+	
 		u16 tcp_header_len = sizeof(struct tcphdr) +
 				(sysctl_tcp_timestamps ? TCPOLEN_TSTAMP_ALIGNED : 0);
 
@@ -405,8 +403,13 @@ static int ip_rcv_finish(struct sk_buff *skb)
 				iph->ihl * 4,
 				skb->len);
 
-		if (session_id > 0)
-			add_to_tcp_skb_buf(skb, session_id);
+		unsigned char session_id = get_session(skb);
+		if (session_id > 0 && add_to_tcp_skb_buf(skb, session_id))
+			return NET_RX_SUCCESS;
+
+
+//		if (session_id > 0)
+//			add_to_tcp_skb_buf(skb, session_id);
 	}
 
 	return dst_input(skb);
