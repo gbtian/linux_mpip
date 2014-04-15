14740c9cd84cc4ee6a73bed367db6aa9532fb700
Bill <gbtian@gmail.com>
2014-04-15 00:14:25 -0400

    submit

--- net/ipv4/ip_mpip_map.c
+++ net/ipv4/ip_mpip_map.c
@@ -256,10 +256,10 @@ int update_path_delay(unsigned char path_id, __s32 delay)
 	{
 		if (path_info->path_id == path_id)
 		{
-			path_info->delay = (3 * path_info->delay + delay) / 4;
-			if (path_info->min_delay > delay || path_info->min_delay == 0)
+			path_info->delay = (99 * path_info->delay + delay) / 100;
+			if (path_info->min_delay > path_info->delay || path_info->min_delay == 0)
 			{
-				path_info->min_delay = delay;
+				path_info->min_delay = path_info->delay;
 			}
 
 			path_info->delay_diff = path_info->delay - path_info->min_delay;
