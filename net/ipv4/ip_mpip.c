4ad60514c7f300b7ac41876b50264dcffd7cd340
bill <gbtian@gmail.com>
2014-06-02 17:36:19 -0400

    submit

--- net/ipv4/ip_mpip.c
+++ net/ipv4/ip_mpip.c
@@ -506,8 +506,8 @@ bool insert_mpip_cm(struct sk_buff *skb, __be32 old_saddr, __be32 old_daddr,
 	send_cm[15] = checksum & 0xff;
 	send_cm[16] = (checksum>>8) & 0xff;
 
-	mpip_log("sending: %s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
-	mpip_log("%d, %d, %d\n", send_cm[15], send_cm[16], checksum);
+	//mpip_log("sending: %s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
+	//mpip_log("%d, %d, %d\n", send_cm[15], send_cm[16], checksum);
 	//print_mpip_cm(&send_mpip_cm);
 
 	skb_put(skb, MPIP_CM_LEN + 1);
