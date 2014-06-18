7d565bef4652c64e4781007b3029e36e59c56a1b
bill <gbtian@gmail.com>
2014-06-18 11:00:56 -0400

    submit

--- net/ipv4/ip_mpip_map.c
+++ net/ipv4/ip_mpip_map.c
@@ -19,6 +19,10 @@ static LIST_HEAD(ss_head);
 static LIST_HEAD(la_head);
 static LIST_HEAD(ps_head);
 
+int global_stat_1;
+int global_stat_2;
+int global_stat_3;
+
 bool is_equal_node_id(unsigned char *node_id_1, unsigned char *node_id_2)
 {
 	int i;
