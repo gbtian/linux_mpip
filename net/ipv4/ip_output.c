5e67cc599a756ba7fe209ba0987da6f49b21a788
bill <gbtian@gmail.com>
2014-08-07 01:01:40 -0400

    submit

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -116,6 +116,9 @@ int ip_local_out(struct sk_buff *skb)
 	int err;
 	struct iphdr *iph = ip_hdr(skb);
 
+	printk("sending: %d, %d, %s, %s, %d\n", iph->id,
+			iph->protocol, __FILE__, __FUNCTION__,	__LINE__);
+
 	if (sysctl_mpip_enabled)
 	{
 		myskb = skb_copy(skb, GFP_ATOMIC);
